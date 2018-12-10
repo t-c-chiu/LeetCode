5b2022c94c4374d04c51bbd583c171b7922d8521
Charles Qiu <charlesqiu@potix.com>
2018-12-10 18:31:00 +0800

    no message

---
+++ src/medium/ProductOfArrayExceptSelf.java
@@ -0,0 +1,56 @@
+package medium;
+
+public class ProductOfArrayExceptSelf {
+
+	public static void main(String[] args) {
+		for (int i : new ProductOfArrayExceptSelf().productExceptSelf(new int[]{1, 2, 3, 4}))
+			System.out.println(i);
+	}
+
+	// 1,1,2,6 * 24,12,4,1
+	// 24,12,8,6
+	public int[] productExceptSelf(int[] nums) {
+		int length = nums.length;
+		int[] result = new int[length];
+		int total = 1;
+		int zeroCount = 0;
+		for (int i = 0; i < length; i++) {
+			int num = nums[i];
+			if (num == 0)
+				zeroCount++;
+			else
+				total *= nums[i];
+		}
+		if (zeroCount > 1)
+			return result;
+		if (zeroCount == 1) {
+			for (int i = 0; i < length; i++) {
+				int num = nums[i];
+				if (num == 0)
+					result[i] = total;
+				else
+					result[i] = 0;
+			}
+		} else {
+			for (int i = 0; i < length; i++)
+				result[i] = total / nums[i];
+		}
+		return result;
+	}
+
+	public int[] productExceptSelf2(int[] nums) {
+		int length = nums.length;
+		int[] result = new int[length];
+		result[0] = 1;
+
+		for (int i = 1; i < length; i++) {
+			result[i] = result[i - 1] * nums[i - 1];
+		}
+		int right = 1;
+		for (int i = length - 1; i >= 0; i--) {
+			result[i] *= right;
+			right *= nums[i];
+		}
+		return result;
+	}
+}
---
+++ src/medium/TopKFrequentElements.java
@@ -0,0 +1,42 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentElements {
+
+	public static void main(String[] args) {
+		TopKFrequentElements topKFrequentElements = new TopKFrequentElements();
+		System.out.println(topKFrequentElements.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2));
+		System.out.println(topKFrequentElements.topKFrequent(new int[]{1, 2}, 2));
+	}
+
+	public List<Integer> topKFrequent(int[] nums, int k) {
+		List<Integer> result = new ArrayList<>(k);
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i : nums) {
+			map.put(i, map.getOrDefault(i, 0) + 1);
+		}
+
+		List<Integer>[] lists = new ArrayList[nums.length + 1];
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			List<Integer> list = lists[entry.getValue()];
+			if (list == null) {
+				list = new ArrayList<>();
+				lists[entry.getValue()] = list;
+			}
+			list.add(entry.getKey());
+		}
+
+		for (int i = nums.length; i > 0 && k > 0; i--) {
+			List<Integer> list = lists[i];
+			if (list == null)
+				continue;
+			result.addAll(list);
+			k -= list.size();
+		}
+		return result;
+	}
+}
